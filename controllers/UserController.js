345979813245de257fc319077e72591f7932668d
pedrohcmoda <phcunha47@gmail.com>
2024-04-21 23:42:57 -0300

    testes de unicidade, retirando mensagens do return, padrao: 2 > request invalida, 1> tudo ok, 0 erro

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const authConfig = require('../config/auth.json');
+const Post = require('../models/Post.js');
 
 function generateToken(params = {}){
     return jwt.sign(params, authConfig.secret, {
@@ -14,59 +15,62 @@ module.exports = {
 
 
     async login(req, res) {
-        const {password, email, isLogged} = req.body;
+        const {password, email} = req.body;
         const user = await User.findOne({where: { email }})
         if(!user) {
             return res.status(200).send({
-                status: 0,
-                message: "E-mail ou senha incorreto"
+                status: 2
             })
         }
         if(!bcrypt.compareSync(password, user.password)){
             return res.status(400).send({
-                status: 0,
-                message: "E-mail ou senha incorreto"
+                status: 2
             })
         }
 
         const user_id = user.id;
-
-        await User.update({
-            isLogged
-        }, {
-            where: {
-                id: user_id
-            }
-        })
-
         user.password = undefined;
         const token = generateToken({id: user_id})
         return res.status(200).send({
             status: 1,
-            message: "Usuario logado com sucesso",
-            user, token
+            user_id, token
         });
     },
-
     async index(req, res){
-        const user = await User.findAll();
-        if( user === "" || user === null){
-            return res.status(200).send({message:"Nenhum usuario cadastrado."})
-        }
-        return res.status(200).send({user});
-    },
-    async store(req, res){
-        const {username, password, email} = req.body
-        const user = await User.create({username, password, email});
-
-        
-        const token = generateToken({id: user.user_id})
-
-        return res.status(200).send({
-            status: 1,
-            message: "Usuario cadastrado com sucesso",
-            user, token
+        const users = await User.findAll({
+          include: {
+            model: Post,
+            as: 'posts'
+          }
         });
+        if( users === "" || users === null){
+          return res.status(200).send({message:"Nenhum usuario cadastrado."})
+        }
+        return res.status(200).send({users});
+      },
+      async indexOne(req, res){
+        const users = await User.findByPk(req.params.user_id, {attributes: {exclude: ['password']}})
+        return res.status(200).send({users}); 
+      },
+
+    async store(req, res){
+        const {username, email, password} = req.body
+        const testeuser = await User.findOne({where: { email }})
+        if(testeuser) {
+            return res.status(200).send({
+                status: 2
+            })
+        }
+        try{
+            await User.create({username, password, email});
+            return res.status(200).send({
+                status: 1,
+            });
+        }catch {
+            return res.status(400).send({
+                status: 2
+            })
+        }
     },
     async update(req, res){
 
@@ -83,7 +87,6 @@ module.exports = {
         })
         return res.status(200).send({
             status: 1,
-            message: "Usuario atualizado com sucesso",
         });
     },
     async delete(req, res){
@@ -95,7 +98,6 @@ module.exports = {
         })
         return res.status(200).send({
             status: 1,
-            message: "Usuario excluido com sucesso",
         });     
     }
 }
