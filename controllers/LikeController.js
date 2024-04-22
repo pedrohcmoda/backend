9c0543fff72ce1a4a178fca5273b472196ed2859
pedrohcmoda <phcunha47@gmail.com>
2024-04-21 23:43:25 -0300

    adaptacao para uso do mesmo endpoint para like e unlike

--- controllers/LikeController.js
+++ controllers/LikeController.js
@@ -5,40 +5,20 @@ module.exports = {
   // Like a post (consider authorization)
   async likePost(req, res) {
     const { post_id } = req.params;
-    const userId = req.user.id; // Assuming user authentication
-
+    const users_id = req.body.user_id;
     try {
-      const existingLike = await Like.findOne({ where: { post_id, userId } });
+      const existingLike = await Like.findOne({ where: { post_id, users_id } });
 
       if (existingLike) {
-        return res.status(400).json({ message: 'You already liked this post' });
+        await existingLike.destroy();
+        return res.status(200).json({ message: 'Post unliked successfully' });
       }
 
-      const newLike = await Like.create({ post_id, userId });
+      const newLike = await Like.create({ post_id, users_id });
       return res.status(201).json({ message: 'Post liked successfully', like: newLike });
     } catch (error) {
       console.error(error);
       return res.status(500).json({ message: 'Error liking post' });
-    }
-  },
-
-  // Unlike a post (consider authorization)
-  async unlikePost(req, res) {
-    const { post_id } = req.params;
-    const userId = req.user.id; // Assuming user authentication
-
-    try {
-      const likeToDelete = await Like.findOne({ where: { post_id, userId } });
-
-      if (!likeToDelete) {
-        return res.status(404).json({ message: 'You haven\'t liked this post yet' });
-      }
-
-      await likeToDelete.destroy();
-      return res.status(200).json({ message: 'Post unliked successfully' });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'Error unliking post' });
     }
   },
 
