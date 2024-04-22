bbfcf156533feae44fdf176cc66cc0e43aff2573
pedrohcmoda <phcunha47@gmail.com>
2024-04-21 23:41:34 -0300

    configuracao de cors

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
-
+const cors = require('cors');
 const routes = require('./routes.js');
 
+
 require('./database/index.js');
+
 const app = express();
+
 
 app.use(express.json());
 
+app.options('*', cors())
+app.use(cors()) 
+
 app.use(routes);
 
-app.listen(3333);
+app.listen(3334);
+
+
+  
